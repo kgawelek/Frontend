71f3c57ca445ceba9a69ab1c3644a97fa5865039
kgawelek <konradgawelek@gmail.com>
2020-04-21 16:37:23 +0200

    dzialajaca gra w kolko i krzyzyk

--- tic-tac-toe/kod.js
+++ tic-tac-toe/kod.js
@@ -1,22 +1,72 @@
 const pola = Array.from(document.querySelectorAll('.plansza div.pola'));
 console.log('pola :', pola);
 
+
 let nazwaGracza = document.querySelector('#gracz');
-let gracz = 0;
+let gracz = 0; // 0 -> runda gracza A, 1 -> runda gracza B
+let wygrany = 'Remis'
+
+mozliwosciWygranych = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+
+function wypiszZwyciezce(wygrany){
+    console.log(wygrany);
+
+    poleWygranego = document.querySelector('#zwyciezca');
+
+    poleWygranego.textContent = wygrany + '!';
+}
+
+let iloscSprawdzen = 0;
+
+function sprawdzWygranego(gracz){
+    
+    for(let i = 0; i < mozliwosciWygranych.length; i++){
+        
+        if(gracz % 2 == 1){ // po zagraniu gracza A, sprawdzamy X
+            if( pola[mozliwosciWygranych[i][0]].innerText == 'X'
+                && pola[mozliwosciWygranych[i][1]].innerText == 'X'
+                && pola[mozliwosciWygranych[i][2]].innerText == 'X'
+                ){
+                    wygrany = 'Wygrał Gracz A';
+                    wypiszZwyciezce(wygrany);
+                }
+        }
+
+        else{
+            // po zagraniu gracza B, sprawdzam O
+            if( pola[mozliwosciWygranych[i][0]].innerText == 'O'
+                && pola[mozliwosciWygranych[i][1]].innerText == 'O'
+                && pola[mozliwosciWygranych[i][2]].innerText == 'O'
+                ){
+                    wygrany = 'Wygrał Gracz B';
+                    wypiszZwyciezce(wygrany);
+                }
+
+        }
+    }
+    iloscSprawdzen++;
+    if(iloscSprawdzen == 9){
+        wypiszZwyciezce(wygrany);
+    }
+}
+
 for(let j = 0; j < 9; j++){
     pola[j].addEventListener('click', function(e){
-        if(gracz % 2 == 0){
+        if(gracz % 2 == 0 && pola[j].innerText == '' && wygrany == 'Remis'){
             pola[j].textContent = 'X';
             gracz++;
             nazwaGracza.textContent = 'Gracz B';
         }
-        else{
+        else if (pola[j].innerText == '' && wygrany == 'Remis'){
             pola[j].textContent = 'O';
             gracz++;
             nazwaGracza.textContent = 'Gracz A';
         }
+
+        sprawdzWygranego(gracz);
     });
 }
+
 
 
 
