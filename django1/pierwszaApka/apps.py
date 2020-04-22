29ff53909c4f7633a2922a104f240cc3d1c52ef1
kgawelek <konradgawelek@gmail.com>
2020-04-22 20:23:39 +0200

    pierwsza aplikacja python Django

---
+++ django1/pierwszaApka/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PierwszaapkaConfig(AppConfig):
+    name = 'pierwszaApka'
---
+++ django1/pierwszaApka/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Film(models.Model):
+    tytul = models.CharField(max_length=32)
+
