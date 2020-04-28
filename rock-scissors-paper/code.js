dccbfca3b27686e8c2f4366aac789c9f2ca508b7
kgawelek <konradgawelek@gmail.com>
2020-04-28 16:37:32 +0200

    działająca gra, dodanie wyników

--- rock-scissors-paper/code.js
+++ rock-scissors-paper/code.js
@@ -4,6 +4,36 @@ let randomChoice = document.querySelector('#random_choice');
 
 
 function checkTheWinner(i, rand){
+    if(i == 0){
+        if(rand == 2 | rand == 3){
+            result.textContent = 'You won!';
+        }
+        else  result.textContent = 'You lost!';
+    }
+    else if(i == 1){
+        if(rand == 0 | rand == 4){
+            result.textContent = 'You won!';
+        }
+        else  result.textContent = 'You lost!';
+    }
+    else if(i == 2){
+        if(rand == 1 | rand == 3){
+            result.textContent = 'You won!';
+        }
+        else  result.textContent = 'You lost!';
+    }
+    else if(i == 3){
+        if(rand == 1 | rand == 4){
+            result.textContent = 'You won!';
+        }
+        else  result.textContent = 'You lost!';
+    }
+    else if(i == 4){
+        if(rand == 0 | rand == 2){
+            result.textContent = 'You won!';
+        }
+        else  result.textContent = 'You lost!';
+    }
 
 }
 
@@ -11,11 +41,23 @@ function addIcon(rand){
     if(rand == 0){
         randomChoice.innerHTML = '<i class="far fa-hand-rock"></i>';
     }
+    else if(rand == 1){
+        randomChoice.innerHTML = '<i class="far fa-hand-paper"></i>';
+    }
+    else if(rand == 2){
+        randomChoice.innerHTML = '<i class="far fa-hand-scissors"></i>';
+    }
+    else if(rand == 3){
+        randomChoice.innerHTML = '<i class="far fa-hand-lizard"></i>';
+    }
+    else if(rand == 4){
+        randomChoice.innerHTML = '<i class="far fa-hand-spock"></i>';
+    }
 }
 
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < 5; i++){
     choices[i].addEventListener('click', function(e){
-        let computerChoice = Math.floor((Math.random() * 4));
+        let computerChoice = Math.floor((Math.random() * 5));
         console.log(computerChoice);
         
         addIcon(computerChoice);
@@ -24,7 +66,7 @@ for(let i = 0; i < 4; i++){
             result.textContent = 'Draw!';
             console.log('draw');
         }
+        else checkTheWinner(i, computerChoice);
         
-
     })
 }
